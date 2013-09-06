f4e03ac9a4019a538df1c574c22e968fd58cfd52
Denis Ciccale <dciccale@gmail.com>
2013-09-06 14:48:28 +0200

    update website

--- reports/report.history.js
+++ reports/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 06 Sep 2013 11:52:21 GMT","total":{"sloc":3046,"maintainability":624.1428781054834},"average":{"sloc":338,"maintainability":"69.35"}}]
+__history = [{"date":"Fri, 06 Sep 2013 11:52:21 GMT","total":{"sloc":3046,"maintainability":624.1428781054834},"average":{"sloc":338,"maintainability":"69.35"}},{"date":"Fri, 06 Sep 2013 12:30:29 GMT","total":{"sloc":3046,"maintainability":624.1432122129628},"average":{"sloc":338,"maintainability":"69.35"}}]
